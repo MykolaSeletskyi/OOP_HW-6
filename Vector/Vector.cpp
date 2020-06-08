da01557f396a99f759f0189d8676d9382913fc48
Микола Селецький <Seletskyi.Mykola@outlook.com>
2020-06-08 23:52:15 +0300

    Добавьте файлы проекта.

---
+++ Vector/Vector.cpp
@@ -0,0 +1,41 @@
+#include "Vector.h"
+
+void Vector::reserve(size_t capacity)
+{
+	int* temp = new int[capacity];
+	for (size_t i = 0; i < capacity; i++)
+	{
+		temp[i] = arr[i];
+	}
+	this->capacity = capacity;
+	delete[]arr;
+	arr = temp;
+}
+
+void Vector::reserve(const Vector& obj)
+{
+	int* arr = new int[obj.capacity];
+	for (size_t i = 0; i < obj.capacity; i++)
+	{
+		arr[i] = obj.arr[i];
+	}
+	this->capacity = obj.capacity;
+}
+
+int& Vector::operator[](size_t number)
+{
+	static int bad;
+	if (number<size)
+	{
+		return arr[number];
+	}
+	return bad;
+}
+
+void Vector::fill(int value)
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		arr[i] = value;
+	}
+}
---
+++ Vector/Vector.h
@@ -0,0 +1,59 @@
+#pragma once
+class Vector
+{
+	static const size_t START_CAPACITY = 5;
+	int* arr = nullptr;
+	size_t capacity;
+	size_t size;
+	void reserve(size_t capacity = START_CAPACITY);//мона поставити size + START_CAPACITY
+	void reserve(const Vector& obj);
+public:
+	Vector(size_t size=0) : Vector(size, 0) {                                                                   };
+	Vector(size_t size, int value) {
+		reserve(size + START_CAPACITY);
+		this->size = size;
+		fill(value);
+	}
+	Vector(const Vector& obj)
+	{
+		size = obj.size;
+		reserve(capacity);
+	}
+	Vector(Vector&& obj)
+	{
+		size = obj.size;
+		arr = obj.arr;
+		capacity = obj.capacity;
+		obj.arr = nullptr;
+		obj.size = 0;
+	}
+	size_t getSize()const
+	{
+		return capacity;
+	}
+	size_t getCapacity()const
+	{
+		return size;
+	}
+	bool empty()const
+	{
+		return size == 0;
+	}
+	void setValue(size_t index, int value) 
+	{
+		if (index<size)
+		{
+			arr[index] = value;
+		}
+	}
+	int &operator [](size_t number);
+	void fill(int value);
+	~Vector()
+	{
+		delete[]arr;
+		arr = nullptr;
+		capacity = 0;
+		size = 0;
+	};
+};
+
